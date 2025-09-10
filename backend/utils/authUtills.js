a3d5807a16bdebd7e9dc23f219bd684e3a1fe018
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-10 10:07:44 +0000

    format auth utils

--- backend/utils/authUtills.js
+++ backend/utils/authUtills.js
@@ -1,19 +1,19 @@
-import bcrypt from "bcryptjs"
-import crypto from "crypto"
-import Token from "../models/token.js"
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import Token from "../models/token.js";
 
-export async function hashPassword (password) {
-  const hashedPassword = await bcrypt.hash(password, 10)
-  return hashedPassword
+export async function hashPassword(password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
 }
 
-export async function generateToken ( userId ) {
-  const token = crypto.randomBytes(32).toString("hex")
-  const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000)
+export async function generateToken(userId) {
+  const token = crypto.randomBytes(32).toString("hex");
+  const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
   await Token.create({
     user: userId,
     token,
-    expiresAt
-  })
-  return token
+    expiresAt,
+  });
+  return token;
 }
