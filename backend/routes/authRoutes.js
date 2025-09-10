88f754fdb7386e9ccad8c0d30f139fa794e893d8
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-10 10:49:09 +0000

    add verify route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import inputValidation from "../middlewares/schemaValidate.js"
 import { userSignupInputValidation } from "../validations/userValidation.js"
-import { signup } from "../controllers/authController.js"
+import { signup, verify } from "../controllers/authController.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup", inputValidation(userSignupInputValidation), signup)
+authRouter.post("/verify", verify)
 
 export default authRouter
