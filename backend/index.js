83cc48d301dec53918e9999cba075e0191b53494
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-10 07:08:21 +0000

    import auth router

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,16 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import connectDB from "./config/db.js"
 
+import authRouter from "./routes/authRoutes.js"
+
 const app = express()
 const port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+
+app.use("/api/v1/auth", authRouter)
 
 app.get("/", (req,res) => {
   res.status(200).send("ok")
