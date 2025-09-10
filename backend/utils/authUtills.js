a4cea5f50c02fbcaa0212931ef89e943058291c7
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-10 10:02:33 +0000

    add mailer

--- backend/utils/authUtills.js
+++ backend/utils/authUtills.js
@@ -1,6 +1,19 @@
 import bcrypt from "bcryptjs"
+import crypto from "crypto"
+import Token from "../models/token.js"
 
 export async function hashPassword (password) {
   const hashedPassword = await bcrypt.hash(password, 10)
   return hashedPassword
+}
+
+export async function generateToken ( userId ) {
+  const token = crypto.randomBytes(32).toString("hex")
+  const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000)
+  await Token.create({
+    user: userId,
+    token,
+    expiresAt
+  })
+  return token
 }
