f58f20a95b3b1082c3f3745544f417d78e7ee7b9
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-10 10:49:40 +0000

    add verify controll

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -35,9 +35,9 @@ export const signup = async (req, res) => {
         html: `<p>Click on the link to verify: <a href="${verifyURL}">${verifyURL}</a>.Or you can copy paste link in your browser</p>`,
       });
     } catch (emailErr) {
-      await Token.deleteMany({ user: user._id })
+      await Token.deleteMany({ user: user._id });
       await User.findByIdAndDelete(user._id);
-      
+
       console.log("Failed to send verification Email", emailErr);
       return res.status(500).json({
         success: false,
@@ -53,6 +53,51 @@ export const signup = async (req, res) => {
   } catch (err) {
     console.error("Error in signup", err);
     return res.status(500).json({
+      error: "Server error. Please try again!",
+    });
+  }
+};
+
+export const verify = async (req, res) => {
+  try {
+    const token = req.query?.token;
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Missing verification token. Please click on the verification link again or signup again",
+      });
+    }
+
+    const dbUser = await Token.findOne({ token });
+    if (!dbUser) {
+      return res.status(404).json({
+        success: false,
+        message:
+          "Token not found. Use the verification link again. If copy pasted then verify again",
+      });
+    }
+
+    if (dbUser.expiresAt < Date.now()) {
+      return res.status(401).json({
+        success: false,
+        message:
+          "Link expired. please signup again to get the new verification link",
+      });
+    }
+
+    await User.findByIdAndUpdate(dbUser.user, { isVerified: true });
+    await Token.findByIdAndDelete(dbUser._id);
+
+    return res.status(200).json({
+      success: true,
+      message: "verification successfull",
+    });
+
+  } catch (err) {
+    console.error("Error in email verification", err);
+    return res.status(500).json({
+      success: false,
       error: "Server error. Please try again!",
     });
   }
