dc515078938ef903460eb705210c1ad79e82fa67
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-10 06:06:41 +0000

    add user signup validation zod schema

---
+++ backend/validations/userValidation.js
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const userSignupInputValidation = z.object({
+  name: z.string().min(3, { error: "name should have at least 3 letters" }).max(40, { error: "name should be under 40 letters" }),
+  username: z.string().trim().max(30, { error: "username is too long. Max allowed length is 30" }),
+  email: z.email(),
+  password: z.string().trim().min(8, { error: "Password must be at least 8 character long" }).max(64, { error: "Password must be under 64 character" })
+})
