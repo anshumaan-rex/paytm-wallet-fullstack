8e41dcc344a872fdb88604ab0067398506fde7d4
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-11 01:58:15 +0000

    add reset password route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import inputValidation from "../middlewares/schemaValidate.js"
-import { userSigninInputValidation, userSignupInputValidation } from "../validations/userValidation.js"
-import { forgotPassword, signin, signup, verify } from "../controllers/authController.js"
+import { userResetPasswordSchema, userSigninInputValidation, userSignupInputValidation } from "../validations/userValidation.js"
+import { forgotPassword, resetPassword, signin, signup, verify } from "../controllers/authController.js"
 
 const authRouter = express.Router()
 
@@ -9,5 +9,6 @@ authRouter.post("/signup", inputValidation(userSignupInputValidation), signup)
 authRouter.post("/verify", verify)
 authRouter.post("/signin", inputValidation(userSigninInputValidation), signin)
 authRouter.post("/forgot-password", forgotPassword)
+authRouter.patch("/reset-password", inputValidation(userResetPasswordSchema), resetPassword)
 
 export default authRouter
