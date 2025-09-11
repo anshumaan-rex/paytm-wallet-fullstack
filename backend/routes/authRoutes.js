370ea10e35680f9ff29e4a579a7db02338b6c088
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-11 01:46:25 +0000

    add forgot password route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import inputValidation from "../middlewares/schemaValidate.js"
 import { userSigninInputValidation, userSignupInputValidation } from "../validations/userValidation.js"
-import { signin, signup, verify } from "../controllers/authController.js"
+import { forgotPassword, signin, signup, verify } from "../controllers/authController.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup", inputValidation(userSignupInputValidation), signup)
 authRouter.post("/verify", verify)
 authRouter.post("/signin", inputValidation(userSigninInputValidation), signin)
+authRouter.post("/forgot-password", forgotPassword)
 
 export default authRouter
