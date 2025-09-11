c256e781a639645cf7f603fcf31e506c23de96f8
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-11 01:17:32 +0000

    add otp generation

--- backend/utils/authUtills.js
+++ backend/utils/authUtills.js
@@ -27,5 +27,6 @@ export async function generateToken(userId) {
 export function generateOtp () {
   const otp = crypto.randomInt(100000, 999999).toString()
   const hashedOtp = crypto.createHash("sha256").update(otp).digest("hex")
-  return hashedOtp
+  const otpExpiresAt = new Date(Date.now() + 5 * 60 * 1000)
+  return { hashedOtp, otp, otpExpiresAt }
 }
