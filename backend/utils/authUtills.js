c9ceb1b8ed00d75d65e69d04bafa44f5e1f2d93c
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-11 01:08:10 +0000

    add otp generation

--- backend/utils/authUtills.js
+++ backend/utils/authUtills.js
@@ -22,3 +22,10 @@ export async function generateToken(userId) {
   });
   return token;
 }
+
+
+export function generateOtp () {
+  const otp = crypto.randomInt(100000, 999999).toString()
+  const hashedOtp = crypto.createHash("sha256").update(otp).digest("hex")
+  return hashedOtp
+}
