99176e50904f2f3f0aa2eb07865b9e88d4670c2f
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-11 06:24:03 +0000

    create user controller and add get users

---
+++ backend/controllers/userController.js
@@ -0,0 +1,44 @@
+import User from "../models/userModel.js";
+
+export const getAllUser = async (req, res) => {
+  const user = req.user;
+  const search = req.query?.search;
+
+  try {
+    let query = {};
+
+    if (search) {
+      const searchString = search.toString();
+      query = {
+        $or: [
+          { username: { $regex: searchString, $options: "i" } },
+          { name: { $regex: searchString, $options: "i" } },
+        ],
+      };
+    }
+
+    const users = await User.find(query);
+    const filteredUsers = users.filter(
+      (usr) => usr._id.toString() !== user._id.toString()
+    );
+
+    if (filteredUsers.length === 0) {
+      return res.status(204).json({
+        success: true,
+        message: search ? "No user matched" : "Currently there are no users",
+        users: [],
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      users: filteredUsers,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server error. Try again!",
+    });
+  }
+};
+
