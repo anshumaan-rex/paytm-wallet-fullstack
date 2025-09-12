c7dd5eaffbbb7eb695c803c82b07b1d2b72b7061
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-12 02:56:47 +0000

    add get particular user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import User from "../models/userModel.js";
 
 export const getAllUser = async (req, res) => {
@@ -42,3 +43,37 @@ export const getAllUser = async (req, res) => {
   }
 };
 
+export const getParticularUser = async (req,res) => {
+  const id = req.params.id
+  if(!id) {
+    return res.status(400).json({
+      success: false,
+      message: "could not find the user without id. Try again!"
+    })
+  }
+  if(!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      success: false,
+      message: "invalid user id. Try again!"
+    })
+  }
+  try{
+    const user = await User.findById(id).select("-otp -otpExpiresAt -email -balance -createdAt -updatedAt")
+    if(!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found. Try again!"
+      })
+    }
+    return res.status(200).json({
+      success: true,
+      user
+    })
+  }catch(err){
+    console.error("Error in getting a particular user", err)
+    return res.status(500).json({
+      success: false,
+      message: "Server error. Try again!"
+    })
+  }
+}
