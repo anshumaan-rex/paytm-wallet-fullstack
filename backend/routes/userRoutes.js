f8e387332e4ecfd18b10694cb63571768fd3ce49
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-12 03:06:15 +0000

    add particular user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { getAllUser } from "../controllers/userController.js";
+import { getAllUser, getParticularUser } from "../controllers/userController.js";
 
 const userRouter = Router()
 
 userRouter.get("/all", isAuthenticated, getAllUser)
+userRouter.get("/:id", isAuthenticated, getParticularUser)
 
 export default userRouter
