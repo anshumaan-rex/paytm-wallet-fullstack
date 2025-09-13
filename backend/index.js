ce02afd49fb00e4a7aca029b81605e9421d3613d
Anshumaan <sentinelspaperrex@gmail.com>
2025-09-13 03:05:17 +0000

    add security middlewares

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import connectDB from "./config/db.js"
+import helmet from "helmet"
+import morgan from "morgan"
 
 import authRouter from "./routes/authRoutes.js"
 import userRouter from "./routes/userRoutes.js"
@@ -12,9 +14,11 @@ import userRouter from "./routes/userRoutes.js"
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(helmet())
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+app.use(morgan("dev"))
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
@@ -22,6 +26,11 @@ app.use("/api/v1/users", userRouter)
 app.get("/", (req,res) => {
   res.status(200).send("ok")
 })
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" })
+})
+
 
 app.use((err,req,res,next) => {
   console.log(err.stack)
